d5f539a02104669353a163cd7bd7611e610463a9
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-01-17 17:36:25 +0100

    Up

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 $(document).ready(function () {
- $('.next').click (
-   function() {
-    var imageActive = $('img.active');
-    imageActive.removeClass('active');
-    imageActive.next().addClass('active')
-  if (imageActive.hasClass('last') == true) {
-    $('img.first').addClass('active');
-    $('img.last').removeClass('active');
-   }
- });
-});
 
-function SwipeNext() {
-}
+ $('.next').click (
+   function SwipeNext() {
+     imageActive = $('img.active');
+     circleActive = $('i.active');
+     imageActive.removeClass('active');
+     imageActive.next().addClass('active');
+     circleActive.removeClass('active');
+     circleActive.next().addClass('active');
+
+     if (imageActive.hasClass('last') == true) {
+       $('img.first').addClass('active');
+       $('img.last').removeClass('active');
+     }
+     if (circleActive.hasClass('last') == true) {
+       $('i.first').addClass('active');
+       $('i.last').removeClass('active');
+     }
+   }
+)
+
+ $('.prev').click (
+   function SwipePrev() {
+     imageActive = $('img.active');
+     circleActive = $('i.active');
+     imageActive.removeClass('active');
+     imageActive.prev().addClass('active');
+     circleActive.removeClass('active');
+     circleActive.prev().addClass('active');
+
+     if (imageActive.hasClass('first') == true) {
+       $('img.last').addClass('active');
+       $('img.first').removeClass('active');
+     }
+     if (circleActive.hasClass('first') == true) {
+       $('i.last').addClass('active');
+       $('i.first').removeClass('active');
+     }
+   }
+)
+
+
+
+
+
+
+
+});
