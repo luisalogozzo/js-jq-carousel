dfb5dc0301221a2a15bc3f0ee667bcfe6db1bf59
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-01-18 12:03:34 +0100

    bonus2

--- main.js
+++ main.js
@@ -1,69 +1,77 @@
 $(document).ready(function () {
+  var img1 = $( "img:eq(0)");
+  var img2 = $( "img:eq(1)");
+  var img3 = $( "img:eq(2)");
+  var img4 = $( "img:eq(3)");
 
- $('.next').click (
-   function() {
-     SwipeNext();
-   }
-)
-
- $('.prev').click (
-   function() {
-     SwipePrev();
-   }
-)
-
-  $(document).keydown(
-    function() {
-      if (event.keyCode == 39) {
-        SwipeNext();
-      }
-    }
+   $('.next').click (
+     function() {
+       SwipeNext();
+     }
   )
-  $(document).keydown(
-    function() {
-      if (event.keyCode == 37) {
-        SwipePrev();
-      }
-    }
+   $('.prev').click (
+     function() {
+       SwipePrev();
+     }
   )
+    $(document).keydown(
+      function() {
+        if (event.keyCode == 39) {
+          SwipeNext();
+        }
+      }
+    )
+    $(document).keydown(
+      function() {
+        if (event.keyCode == 37) {
+          SwipePrev();
+        }
+      }
+    )
 
-function SwipeNext() {
-  imageActive = $('img.active');
-  circleActive = $('i.active');
-  imageActive.removeClass('active');
-  imageActive.next().addClass('active');
-  circleActive.removeClass('active');
-  circleActive.next().addClass('active');
+  $('.nav i').click(function () {
+    var imageActive = $('img.active');
+    var circleActive = $('.nav i');
+    circleActive.removeClass('active');
+    imageActive.removeClass('active');
+    $(this).addClass('active');
+    if ($(this).index() == 0) {
+      img1.addClass('active');
+    } else if ($(this).index() == 1) {
+      img2.addClass('active');
+    } else if ($(this).index() == 2) {
+      img3.addClass('active');
+    } else if ($(this).index() == 3) {
+      img4.addClass('active');
+    }
+  });
+  function SwipeNext() {
+    var imageActive = $('.images img.active');
+    var circleActive = $('.nav i.active');
+    imageActive.removeClass('active');
+    circleActive.removeClass('active');
 
-  if (imageActive.hasClass('last') == true) {
-    $('img.first').addClass('active');
-    $('img.last').removeClass('active');
+    if (imageActive.hasClass('last') == true) {
+      $('img.first').addClass('active');
+      $('i.first').addClass('active');
+    } else {
+      imageActive.next().addClass('active');
+      circleActive.next().addClass('active');
+    }
+
   }
-  if (circleActive.hasClass('last') == true) {
-    $('i.first').addClass('active');
-    $('i.last').removeClass('active');
+  function SwipePrev() {
+    var imageActive = $('.images img.active');
+    var circleActive = $('.nav i.active');
+    imageActive.removeClass('active');
+    circleActive.removeClass('active');
+    if (imageActive.hasClass('first') == true) {
+      $('img.last').addClass('active');
+      $('i.last').addClass('active');
+    } else {
+      imageActive.prev().addClass('active');
+      circleActive.prev().addClass('active');
+    }
   }
-}
-
-function SwipePrev() {
-  imageActive = $('img.active');
-  circleActive = $('i.active');
-  imageActive.removeClass('active');
-  imageActive.prev().addClass('active');
-  circleActive.removeClass('active');
-  circleActive.prev().addClass('active');
-
-  if (imageActive.hasClass('first') == true) {
-    $('img.last').addClass('active');
-    $('img.first').removeClass('active');
-  }
-  if (circleActive.hasClass('first') == true) {
-    $('i.last').addClass('active');
-    $('i.first').removeClass('active');
-  }
-}
-
-
-
 
 });
